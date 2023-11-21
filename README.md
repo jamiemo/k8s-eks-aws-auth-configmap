a521220e3415393e2cddb318dc31faae632ccef1
jamiemo <jamie@jamiemorrison.com>
2023-11-21 17:46:10 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # k8s-eks-aws-auth-configmap
+
+There does not appear to be a readily available and complete example of [identity mapping of IAM roles](https://docs.aws.amazon.com/eks/latest/userguide/add-user-role.html) for an existing EKS cluster using terraform.
+
+Typically this would be achieved via [eksctl](https://eksctl.io/usage/iam-identity-mappings/):
+```bash
+eksctl get iamidentitymapping --cluster <clusterName> --region=<region>
+```
+
+If using [manage_aws_auth_configmap](https://github.com/terraform-aws-modules/terraform-aws-eks#input_manage_aws_auth_configmap) with the  [terraform-aws-eks](https://github.com/terraform-aws-modules/terraform-aws-eks) module, the aws-auth config map can be managed as per this [example](https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest#usage).
+
+This terraform searches for the existing EKS cluster via tags, and then creates a data source for the cluster and the aws-auth config map. Using distinct(concat()) ensures that the config map is only updated once, even when using `terraform apply` multiple times.
+
+Before:
+```bash
+kubectl get configmap aws-auth -n kube-system -o yaml
+apiVersion: v1
+data:
+  mapRoles: |
+    - groups:
+      - system:bootstrappers
+      - system:nodes
+      rolearn: arn:aws:iam::999999999999:role/jamiemo-dev-NodeInstanceRole-QaTaYdYbA5Hg
+      username: system:node:{{EC2PrivateDNSName}}
+  mapUsers: |
+    []
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+```
+
+Apply the example:
+```bash
+terraform init
+terraform apply
+```
+
+After:
+```bash
+kubectl get configmap aws-auth -n kube-system -o yaml
+apiVersion: v1
+data:
+  mapRoles: |
+    - groups:
+      - system:bootstrappers
+      - system:nodes
+      rolearn: arn:aws:iam::999999999999:role/jamiemo-dev-NodeInstanceRole-QaTaYdYbA5Hg
+      username: system:node:{{EC2PrivateDNSName}}
+    - groups:
+      - system:masters
+      rolearn: arn:aws:iam::999999999999:role/service-role
+      username: service-role
+  mapUsers: |
+    []
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+```
---
+++ main.tf
@@ -0,0 +1,94 @@
+# 1. Find cluster based on tags
+data "aws_resourcegroupstaggingapi_resources" "cluster" {
+  resource_type_filters = ["eks:cluster"]
+
+  tag_filter {
+    key    = "Environment"
+    values = ["dev"]
+  }
+
+  tag_filter {
+    key    = "Project"
+    values = ["jamiemo"]
+  }
+}
+
+# 2. Get aws-auth config map data source
+data "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+}
+
+# 3. Extract cluster name
+locals {
+  cluster_arn      = data.aws_resourcegroupstaggingapi_resources.cluster.resource_tag_mapping_list[0].resource_arn
+  cluster_name     = regex("^arn:aws:eks:.+:\\d+:cluster\\/(.+)$", local.cluster_arn)[0] 
+}
+
+# 4. Create EKS data sources with cluster name
+data "aws_eks_cluster" "eks" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = local.cluster_name
+}
+
+# 5. Create kubernetes provider
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+# 6. Create IAM role and policy e.g. codebuild
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "service_role" {
+  name               = "service-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# 7. Create servicec role permissions
+locals {
+  new_role_yaml = <<-EOF
+    - groups:
+      - system:masters
+      rolearn: ${aws_iam_role.service_role.arn}
+      username: ${aws_iam_role.service_role.name}
+    EOF
+}
+
+# 8. Update aws-auth configmap
+resource "kubernetes_config_map_v1_data" "aws_auth" {
+  force = true
+
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    # Convert to list, make distinict to remove duplicates, and convert to yaml as mapRoles is a yaml string.
+    # replace() remove double quotes on "strings" in yaml output.
+    # distinct() only apply the change once, not append every run.
+    mapRoles = replace(yamlencode(distinct(concat(yamldecode(data.kubernetes_config_map.aws_auth.data.mapRoles), yamldecode(local.new_role_yaml)))), "\"", "")
+  }
+
+  lifecycle {
+    ignore_changes = []
+  }
+}
